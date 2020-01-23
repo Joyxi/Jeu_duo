a113de974db0d4bcf13087c059e981b4522fabb8
emma <>
2020-01-23 10:58:26 +0100

    Update jeu.c

--- jeu.c
+++ jeu.c
@@ -1,1 +1,43 @@
 #include <stdio.h>
+
+int main () {
+	
+	int pdv;
+	pdv = 150;
+	
+	int pdvMoi;
+	pdvMoi = 150;
+	
+	int choix;
+	
+	printf ("Niveau de vie de l'ennemi: %d\n",pdv);
+	printf ("Mon niveau de vie: %d\n",pdvMoi);
+	
+	printf("Debut du combat\n");
+	
+	printf("Vous lancez une attaque contre le monstre\n");
+	printf("Vous avez touche le monstre! -10p\n");
+	pdv = pdv-10;
+	printf("Niveau de vie de l'ennemi: %d\n",pdv);
+	
+	printf("Le monstre vous attaque. Attaquer (1) ou se defendre (2) ?
+	scanf("%d",&
+	
+	
+	while (pdv>0 || pdvMoi>0){
+	
+		
+		printf("Vous avez touche le monstre! -10p\n");
+		pdv = pdv-10;
+		printf("Niveau de vie de l'ennemi: %d\n",pdv);
+	
+	
+		printf("Le monstre vous a touche! -10p\n");
+		pdvMoi = pdvMoi-10;
+		printf("Mon niveau de vie: %d\n",pdvMoi);
+		
+	}
+	
+	return 0;
+	
+}
